ff494fc6252b2fb801911c2a1d654a287b236825
Joel3030 <joelencarnacion07@gmail.com>
2022-09-27 19:22:37 -0400

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { join } from 'path';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault],
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+    UserModule,
+  ],
+})
+export class AppModule {}
---
+++ src/user/user.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserInput } from './dto/create-user.input';
+import { UpdateUserInput } from './dto/update-user.input';
+
+@Injectable()
+export class UserService {
+  private readonly users = [
+    {
+      id: 1,
+      username: 'admin',
+      password: '1234',
+    },
+    {
+      id: 2,
+      username: 'user',
+      password: '1234',
+    },
+  ];
+
+  create(createUserInput: CreateUserInput) {
+    return createUserInput;
+  }
+
+  findAll() {
+    return this.users;
+  }
+
+  findOne(username: string) {
+    return this.users.find((user) => user.username === username);
+  }
+
+  update(id: number, updateUserInput: UpdateUserInput) {
+    return `This action updates a #${id} user ${updateUserInput}`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
